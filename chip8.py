2486237a0f33b3ec248caf54df1e87a3e1ea4a0a
Anthony Gedeon <anthony.gedeonn@gmail.com>
2021-03-30 20:20:07 -0500

    added beginner instructions

--- chip8.py
+++ chip8.py
@@ -6,3 +6,22 @@ pygame.init()
 WIDTH = 500 
 HEIGHT = 300
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+class CPU:
+    def __init__(self):
+        self.program_counter = 0x000
+
+    def INST_CLR(self):
+        pass
+
+    def INST_JMP(self):
+        pass
+
+    def set_VX(self):
+        pass
+
+    def set_reg_I(self):
+        pass
+
+    def INST_DRW(self):
+        pass
