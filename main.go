a6d1bbc18dd1cb09f461c53d4d152372cefdaa4a
Anthony Gedeon <anthony.gedeonn@gmail.com>
2021-07-24 19:09:03 -0500

    barebones cli support

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/urfave/cli/v2"
 )
 
 const (
@@ -21,11 +23,55 @@ type vm struct {
 
 func main() {
 
-	cpu.ram.LoadProgram("roms/KALEID")
+	app := &cli.App{
+		Name:    "chip8",
+		Usage:   "chip8 [command]",
+		Version: "0.1.0",
 
-	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
-	ebiten.SetWindowTitle("CHIP-8")
-	if err := ebiten.RunGame(&vm{}); err != nil {
+		Flags: []cli.Flag{
+
+			&cli.BoolFlag{
+				Name:    "debug",
+				Aliases: []string{"d"},
+				Usage:   "Debug running program",
+			},
+
+			&cli.BoolFlag{
+				Name:    "disassemble",
+				Aliases: []string{"disasm"},
+				Usage:   "Print out disassembly of program",
+			},
+
+		},
+
+		Action: func(c *cli.Context) error {
+			return nil	
+		},
+
+		Commands: []*cli.Command{
+			{
+				Name:    "run",
+				Aliases: []string{"r"},
+				Usage:   "run `path/to/rom`",
+				Action: func(c *cli.Context) error {
+
+					rom := c.Args().First()
+					cpu.ram.LoadProgram(rom)
+
+					ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+					ebiten.SetWindowTitle("CHIP-8")
+					if err := ebiten.RunGame(&vm{}); err != nil {
+						return err
+					}
+
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
 		log.Fatal(err)
 	}
 }
