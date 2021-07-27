7bf5462353b233dd46f2f63142aeecf49f6dd77c
Anthony Gedeon <anthony.gedeonn@gmail.com>
2021-07-26 21:57:07 -0500

    feat: disassembler flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -16,43 +17,57 @@ const (
 var cpu = NewCPU()
 
 type vm struct {
-	cpu     CPU
 	display Display
-	ram     Memory
 }
 
 func main() {
 
 	app := &cli.App{
 		Name:    "chip8",
-		Usage:   "chip8 [command]",
+		Usage:   "chip8 [command] [flag]...",
 		Version: "0.1.0",
 
 		Flags: []cli.Flag{
-
-			&cli.BoolFlag{
-				Name:    "debug",
-				Aliases: []string{"d"},
-				Usage:   "Debug running program",
-			},
-
 			&cli.BoolFlag{
 				Name:    "disassemble",
-				Aliases: []string{"disasm"},
-				Usage:   "Print out disassembly of program",
+				Aliases: []string{"d"},
+				Usage:   "Print out disassembly of `rom`",
 			},
-
 		},
 
 		Action: func(c *cli.Context) error {
-			return nil	
+			if c.Bool("disassemble") {
+				rom := c.Args()
+
+				if rom.Len() <= 0 {
+					return fmt.Errorf("choose a rom to disassemble")
+				}
+
+				file, err := os.Open(rom.First())
+				if err != nil {
+					return err
+				}
+
+				bytes, err := cpu.ram.Read(file)
+				if err != nil {
+					return err
+				}
+
+				cpu.ram.LoadProgram(rom.First())
+				for i := 0; i < len(bytes); i++ {
+					Disassemble(cpu)
+				}
+			} else {
+				fmt.Printf("Usage: chip8 [command] [flag]...\nTry 'chip8 --help' for more information.\n")
+			}
+
+			return nil
 		},
 
 		Commands: []*cli.Command{
 			{
-				Name:    "run",
-				Aliases: []string{"r"},
-				Usage:   "run `path/to/rom`",
+				Name:  "run",
+				Usage: "run `path/to/rom`",
 				Action: func(c *cli.Context) error {
 
 					rom := c.Args().First()
