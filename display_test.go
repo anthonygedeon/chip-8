e6985b46cc49175658b5a0c95c6ee7efc21c42d6
Anthony Gedeon <anthony.gedeonn@gmail.com>
2021-07-26 21:55:39 -0500

    add test case for Clear

--- display_test.go
+++ display_test.go
@@ -1,1 +1,19 @@
 package main
+
+import "testing"
+
+func TestClear(t *testing.T) {
+
+	display := Display{}
+
+	display.Clear()
+
+	for i := 0; i < len(display.gfx); i++ {
+		for j := 0; j < len(display.gfx[i]); j++ {
+			if display.gfx[i][j] != 0 {
+				t.Errorf("wanted %[1]q, got %[1]q", display.gfx)
+			}
+		}
+	}
+
+}
