1d6620658770ad64d4798db502b1866c30f4ad7f
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-02-23 17:49:03 -0600

    update naming

--- src/memory.rs
+++ src/memory.rs
@@ -2,31 +2,44 @@ use std::fs::{self, File};
 use std::io;
 
 /// The amount of memory that the CHIP-8 can hold
-pub const MAX_THRESHOLD: usize = 4096;
+const MAX_RAM: usize = 4096;
+
+/// CHIP-8 programs start at location 0x200
+const END_OF_RESERVED: usize = 0x200;
 
 #[derive(Debug)]
 pub struct Memory {
-    pub ram: [u16; MAX_THRESHOLD],
+    /// CHIP-8 internal memory capped at 4096 bytes.
+    pub ram: [u16; MAX_RAM],
 }
 
 impl Memory {
 
-    /// Load the `binary` into memory  
-    pub fn load_binary(&mut self, binary: &str) -> io::Result<()> {
-        let bytes = self.read_binary(binary)?;
-        let offset = 512;
+    pub fn new() -> Self {
+        Self { ram: [0; MAX_RAM] }     
+    }
+
+    /// Load the `rom` into memory.
+    pub fn load_rom(&mut self, rom: &str) -> io::Result<()> {
+        let bytes = self.read_rom(rom)?;
         for (i, opcode) in bytes.iter().enumerate() {
-            self.ram[offset + i] = *opcode as u16;
+            self.ram[END_OF_RESERVED + i] = *opcode as u16;
         }
 
         Ok(())
     }
     
-    /// Read the entire binary of a file
-    fn read_binary(&self, binary: &str) -> io::Result<Vec<u8>> {
-        let mut f = fs::read(binary)?;
+    /// Read the entire rom. 
+    fn read_rom(&self, rom: &str) -> io::Result<Vec<u8>> {
+        let mut f = fs::read(rom)?;
         Ok(f) 
     }
     
 }
 
+#[cfg(test)]
+mod test {
+   use super::*;
+    
+
+}
