f96e59825ae27758fc087e995580f5268192f79c
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-02-23 12:33:58 -0600

    add keyboard inputs

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,27 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::{Color, PixelFormatEnum};
 
+use std::thread;
 use std::time::Duration;
 
 mod memory;
 mod display;
 mod cpu;
+mod vm;
+
+const WIDTH: u32 = 640;
+const HEIGHT: u32 = 480;
+const WINDOW_TITLE: &str = "CHIP-8";
 
 fn main() -> Result<(), String> {
-    let mut cpu = cpu::Cpu::new();
+    let cpu = cpu::Cpu::new();
+    let mut vm = vm::VirtualMachine{ cpu };
 
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("Chip 8", 640, 480)
+        .window(WINDOW_TITLE, WIDTH, HEIGHT)
         .position_centered()
         .opengl()
         .build()
@@ -66,16 +73,33 @@ fn main() -> Result<(), String> {
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-
+                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
+                Event::KeyDown { keycode: Some(Keycode::Num1), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::Num2), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::Num3), .. } => (),  
+                Event::KeyDown { keycode: Some(Keycode::Num4), .. } => (),  
+                Event::KeyDown { keycode: Some(Keycode::Num5), .. } => (),  
+                Event::KeyDown { keycode: Some(Keycode::Num6), .. } => (),  
+                Event::KeyDown { keycode: Some(Keycode::Num7), .. } => (),  
+                Event::KeyDown { keycode: Some(Keycode::Num9), .. } => (),  
+                Event::KeyDown { keycode: Some(Keycode::Q), .. } => (),  
+                Event::KeyDown { keycode: Some(Keycode::W), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::E), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::R), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::A), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::S), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::S), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::D), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::F), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::Z), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::X), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::C), .. } => (), 
+                Event::KeyDown { keycode: Some(Keycode::V), .. } => (), 
                 _ => {}
             }
         }
-        cpu.cycle();
+
+        vm.run();
         canvas.present();
         
         for (mut i, row) in cpu.display.grid.iter().enumerate() {
@@ -93,7 +117,7 @@ fn main() -> Result<(), String> {
             }
         }
 
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+        thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
     }
 
     Ok(())
