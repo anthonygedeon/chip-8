25ac68ac5071e76590474fbaece4dbdfaadb6d2d
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-03-26 15:05:31 -0500

    progress with memory map

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+#define ARRAY_LEN(x) (sizeof(x) / sizeof(x[0]))
+
+typedef struct memory_map memory_map;
+
+struct memory_map {
+        unsigned char ram[4095];
+};
+
+void memory_map_read_rom(memory_map m_map) {
+        
+}
+
+void memory_map_write_rom(memory_map m_map) {
+
+}
+
 void main(void) {
-        printf("CHIP-8");
+
+    memory_map m_map = {};
+
 }
