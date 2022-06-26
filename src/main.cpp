25c4c0583191ce03c01449266dae1a30886e06a5
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-26 16:06:15 -0500

    feat: bare-bones disassembled instructions

--- src/main.cpp
+++ src/main.cpp
@@ -29,41 +29,86 @@ class MemoryMap {
 };
 
 class CPU {
-    
-    void fetch_opcode() {
-        
-    }
+    void fetch_opcode() {}
 
-    public:
-        uint16_t pc;
-        uint8_t sp;
-       
-        std::array<uint8_t, 0xF> v_register;
-        std::array<uint16_t, 16> stack_register;
+   public:
+    uint16_t pc;
+    uint8_t sp;
 
-        uint16_t i_register;
+    std::array<uint8_t, 0xF> v_register;
+    std::array<uint16_t, 16> stack_register;
 
-        uint8_t delay_timer_register;
-        uint8_t sound_timer_register;
-     
+    uint16_t i_register;
+
+    uint8_t delay_timer_register;
+    uint8_t sound_timer_register;
 };
 
 int main() {
-    MemoryMap m_map = { 0 };
-    CPU cpu = { MIN_MEM, 0, 0, 0, 0, 0, 0 };  
+    MemoryMap m_map = {0};
+    CPU cpu = {MIN_MEM, 0, 0, 0, 0, 0, 0};
 
     m_map.load_file("IBMLOGO");
 
-    uint16_t opcode = (m_map.ram[cpu.pc] << 8) | (m_map.ram[cpu.pc+1]);
-    std::cout << cpu.pc << "\n";
-    std::cout << opcode << "\n";
-    //std::cout << "[";
-    //for (int i{0}; i < m_map.ram.size(); i++) {
-        //std::cout << +m_map.ram[i] << " ";
-    //}
-    //std::cout << "]\n";
+    for (;;) {
+        uint16_t opcode = (m_map.ram[cpu.pc] << 8) | (m_map.ram[cpu.pc + 1]);
 
-    
+        // TODO: disassemble cpu for debugging purposes
+        switch (opcode & 0xF000) {
+            case 0x0: {
+                switch (opcode & 0x00FF) {
+                    case 0xE0: {
+                        std::cout << "cls\n";
+                        cpu.pc += 2;
+                        break;
+                    }
+
+                    case 0xEE:
+                        cpu.pc = cpu.stack_register[16];
+                        cpu.sp--;
+                        break;
+                }
+            }
+            case 0x1: {
+                uint16_t nnn = opcode & 0x0FFF;
+                std::cout << "JP " << nnn << "\n";
+                cpu.pc = nnn;
+                break;
+            }
+            case 0x6: {
+                uint8_t x = opcode & 0x0F00;
+                uint8_t kk = opcode & 0x00FF;
+                std::cout << "LD V[" << x << "], " << kk;
+                cpu.v_register[x] = kk;
+                break;
+            }
+            case 0x7: {
+                uint8_t x = opcode & 0x0F00;
+                uint8_t kk = opcode & 0x00FF;
+                std::cout << "LD V[" << x << "], " << kk;
+                cpu.v_register[x] += kk;
+                cpu.pc += 2;
+                break;
+            }
+            case 0xA: {
+                uint16_t nnn = opcode & 0x0FFF;
+                std::cout << "LD I, " << nnn << "\n";
+                cpu.i_register = nnn;
+                cpu.pc += 2;
+                break;
+            }
+            case 0xD: {
+                std::cout << "DRW Vx, Vy, nibble";
+                break;
+            }
+        }
+    }
+
+    // std::cout << "[";
+    // for (int i{0}; i < m_map.ram.size(); i++) {
+    // std::cout << +m_map.ram[i] << " ";
+    //}
+    // std::cout << "]\n";
 
     return 0;
 }
