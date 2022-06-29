e10214500b4d74b352b0d0c2b486378cc83fc50b
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-06-28 22:12:01 -0500

    fix: issue with missing bytes from ram

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,18 @@ class MemoryMap {
     std::array<uint8_t, MAX_MEM> ram;
 
     int load_file(std::string filename) {
-        std::ifstream inf{"roms/" + filename, std::ios::in};
+        std::ifstream inf{"roms/" + filename, std::ios::in | std::ios::binary};
         if (!inf) {
             std::cout << "failed to read " << filename << " from disk\n";
             return 1;
         }
 
         int i = MIN_MEM;
-        while (inf) {
+        while (!inf.eof()) {
             uint8_t mem_byte{};
-            inf >> mem_byte;
+            // TODO: consider using ::get() to preserve bytes instead of
+            // operator>> removing whitespace chars
+            inf >> std::noskipws >> mem_byte;
             this->ram[i] = mem_byte;
             i++;
         }
@@ -41,34 +43,32 @@ class CPU {
 
     uint8_t delay_timer_register;
     uint8_t sound_timer_register;
-    
+
     uint16_t fetch_opcode(std::array<uint8_t, MAX_MEM> ram) {
         return (ram[this->pc] << 8) | (ram[this->pc + 1]);
     }
 };
 
 int main() {
-    MemoryMap m_map = { 0 };
-    CPU cpu = { MIN_MEM, 0, 0, 0, 0, 0, 0 };
+    MemoryMap m_map = {0};
+    CPU cpu = {MIN_MEM, 0, 0, 0, 0, 0, 0};
 
     m_map.load_file("IBMLOGO");
 
     for (;;) {
         uint16_t opcode = cpu.fetch_opcode(m_map.ram);
 
-        uint8_t x = opcode & 0x0F00;
-        uint8_t y = opcode & 0x00F0;
+        uint8_t x = (opcode & 0x0F00) >> 8;
+        uint8_t y = (opcode & 0x00F0) >> 4;
         uint8_t n = opcode & 0x000F;
         uint8_t nn = opcode & 0x00FF;
         uint16_t nnn = opcode & 0x0FFF;
-
-        std::cout << std::hex << opcode << "\n";
 
         switch (opcode & 0xF000) {
             case 0x0000: {
                 switch (opcode & 0x00FF) {
                     case 0xE0: {
-                        std::cout << "cls\n";
+                        std::cout << "CLS\n";
                         cpu.pc += 2;
                         break;
                     }
@@ -87,13 +87,15 @@ int main() {
                 break;
             }
             case 0x6000: {
-                std::cout << "LD V[" << std::hex <<+x << "], " << std::hex << +nn << "\n";
+                std::cout << "LD V[" << std::hex << +x << "], " << std::hex
+                          << +nn << "\n";
                 cpu.v_register[x] = nn;
                 cpu.pc += 2;
                 break;
             }
             case 0x7000: {
-                std::cout << "LD V[" << std::hex <<+x << "], " << std::hex << +nn << "\n";
+                std::cout << "LD V[" << std::hex << +x << "], " << std::hex
+                          << +nn << "\n";
                 cpu.v_register[x] += nn;
                 cpu.pc += 2;
                 break;
