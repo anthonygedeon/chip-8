c33e61921e80d65997a0a450f71097b363505d33
Anthony Gedeon <anthony.gedeonn@gmail.com>
2022-09-28 19:42:17 -0500

    style: rustfmt

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ use sdl2::pixels::{Color, PixelFormatEnum};
 use std::thread;
 use std::time::Duration;
 
+mod cpu;
+mod display;
 mod keyboard;
 mod memory;
-mod display;
-mod cpu;
 
 const WINDOW_WIDTH: u32 = 640;
 const WINDOW_HEIGHT: u32 = 480;
@@ -51,7 +51,7 @@ fn main() -> Result<(), String> {
             }
         }
     })?;
-    
+
     // create black texture
     texture2.with_lock(None, |buffer: &mut [u8], pitch: usize| {
         for y in 0..256 {
@@ -65,40 +65,91 @@ fn main() -> Result<(), String> {
     })?;
 
     canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.set_scale(WINDOW_WIDTH as f32 / 64.0, WINDOW_HEIGHT as f32 / 32.0).expect("could not scale window");
+    canvas
+        .set_scale(WINDOW_WIDTH as f32 / 64.0, WINDOW_HEIGHT as f32 / 32.0)
+        .expect("could not scale window");
     canvas.clear();
 
     let mut event_pump = sdl_context.event_pump()?;
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
-                Event::KeyDown { keycode: Some(Keycode::Num1), .. } => (), 
-                Event::KeyDown { keycode: Some(Keycode::Num2), .. } => (), 
-                Event::KeyDown { keycode: Some(Keycode::Num3), .. } => (), 
-                Event::KeyDown { keycode: Some(Keycode::Num4), .. } => (), 
-                Event::KeyDown { keycode: Some(Keycode::Num5), .. } => (), 
-                Event::KeyDown { keycode: Some(Keycode::Num6), .. } => (), 
-                Event::KeyDown { keycode: Some(Keycode::Num7), .. } => (), 
-                Event::KeyDown { keycode: Some(Keycode::Num8), .. } => (), 
-                Event::KeyDown { keycode: Some(Keycode::Num9), .. } => (), 
-                Event::KeyDown { keycode: Some(Keycode::A), .. } => (),
-                Event::KeyDown { keycode: Some(Keycode::B), .. } => (),
-                Event::KeyDown { keycode: Some(Keycode::C), .. } => (),
-                Event::KeyDown { keycode: Some(Keycode::D), .. } => (),
-                Event::KeyDown { keycode: Some(Keycode::E), .. } => (),
-                Event::KeyDown { keycode: Some(Keycode::F), .. } => (),
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                Event::KeyDown {
+                    keycode: Some(Keycode::Num1),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Num2),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Num3),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Num4),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Num5),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Num6),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Num7),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Num8),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Num9),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::A),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::B),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::C),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::D),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::E),
+                    ..
+                } => (),
+                Event::KeyDown {
+                    keycode: Some(Keycode::F),
+                    ..
+                } => (),
                 _ => {}
             }
         }
-        
+
         cpu.cycle();
         canvas.present();
-        
+
         for (i, row) in cpu.display.grid.iter().enumerate() {
             for (j, _) in row.iter().enumerate() {
-                let rectangle = sdl2::rect::Rect::new(j as i32, i as i32 , 10, 10);
-                
+                let rectangle = sdl2::rect::Rect::new(j as i32, i as i32, 10, 10);
+
                 // draw white cell
                 if cpu.display.grid[i][j] == 1 {
                     canvas.copy(&texture, None, Some(rectangle))?;
